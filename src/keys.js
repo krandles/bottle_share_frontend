d13433c685b373c48b3493a65867418e8ba6b950
krandles <krandles@gmail.com>
2018-05-08 00:40:47 -0400

    adding api keys for deployment

---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const filestackKey = 'A7fNbqUilSoSYVuX9z7Zxz'; // for Events
+export const filestackKeyBeers = 'Acu94EFL1STGYvkM6a8usz'; // for Beers
+export const googleMapsKey = 'AIzaSyCI6MVNeV4DmvZLr2iH_nOuHmde3HTXk7E';
