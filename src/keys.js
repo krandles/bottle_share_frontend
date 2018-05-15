93a00735e28518823a84fbec4e9e80d897f6dfee
krandles <krandles@gmail.com>
2018-05-14 23:15:20 -0400

    merged heroku_build branch, removed keys.js

--- src/keys.js
+++
@@ -1,3 +0,0 @@
-export const filestackKey = 'A7fNbqUilSoSYVuX9z7Zxz'; // for Events
-export const filestackKeyBeers = 'Acu94EFL1STGYvkM6a8usz'; // for Beers
-export const googleMapsKey = 'AIzaSyCI6MVNeV4DmvZLr2iH_nOuHmde3HTXk7E';
